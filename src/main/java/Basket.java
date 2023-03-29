9c6fd71cb038766dce0716ef99f93d3c32fc0d77
KrozhDev <roko.mdn@gmail.com>
2023-03-29 11:43:14 +0300

    First commit

---
+++ src/main/java/Basket.java
@@ -0,0 +1,129 @@
+
+import java.io.*;
+import java.util.Arrays;
+public class Basket implements Serializable {
+
+    private long[] prices;
+    private String[] goods;
+    private long[] quantity;
+
+    public static final long serialVersionUID = 1L;
+
+
+    public Basket() {
+        goods = new String[]{"Хлеб", "Яблоки", "Молоко"};
+        prices = new long[]{100, 200, 300};
+        quantity = new long[]{0,0,0};
+    }
+    public Basket(String[] goods, long[] prices) {
+        this.goods = goods;
+        this.prices = prices;
+    }
+
+    public String[] getGoods() {
+        return this.goods;
+    }
+
+    public long[] getPrices() {
+        return this.prices;
+    }
+
+    public void addToCart(long productNum, long amount) {
+        this.quantity[(int) (productNum-1)] = amount;
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина");
+
+        long sum = 0;
+        for (int i = 0; i < this.goods.length; i++) {
+            sum = sum + this.prices[i] * this.quantity[i];
+            if (this.quantity[i] != 0) {
+                System.out.println(this.goods[i] + " цена: "
+                        + this.prices[i]
+                        +" рублей, всего " + this.quantity[i] + " шт., итого: "
+                        + this.quantity[i]*this.prices[i] + " рублей");
+            }
+
+        }
+        System.out.println();
+        System.out.println("Общая стоимость " + sum + " рублей");
+    }
+
+    public void saveTxt(File textFile) throws RuntimeException {
+        System.out.println("Сохраняю корзину в файл");
+
+        try(PrintWriter out = new PrintWriter(textFile.getPath())) {
+
+            for (String good: this.goods) {
+                out.print(good + " ");
+            }
+            out.println();
+
+            for (long price: this.prices) {
+                out.print(price + " ");
+            }
+            out.println();
+
+            for (long amount: this.quantity) {
+                out.print(amount + " ");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // выгрузка из файла
+    public static Basket loadFromTxtFile(File textFile) throws RuntimeException {
+        FileInputStream fos = null;
+
+        Basket basket = new Basket();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
+
+            String goodsStr = bufferedReader.readLine();
+            String pricesStr = bufferedReader.readLine();
+            String quantitiesStr = bufferedReader.readLine();
+
+            basket.goods = goodsStr.split(" ");
+            String[] arrPrices = pricesStr.split( " ");
+            basket.prices = Arrays.stream(arrPrices).
+                    map(Long::parseLong).
+                    mapToLong(Long::longValue).
+                    toArray();
+            String[] arrQuantities = quantitiesStr.split(" ");
+            basket.quantity = Arrays.stream(arrQuantities).
+                    map(Long::parseLong).
+                    mapToLong(Long::longValue).
+                    toArray();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return basket;
+    }
+
+    public void saveBin(File file) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(this);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static Basket loadFromBinFile(File file) throws ClassNotFoundException {
+        Basket basket = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            basket = (Basket) ois.readObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return basket;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,70 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        File textFile = new File("./out/basket.bin");
+
+        int productNumber = 0;
+        int productCount = 0;
+        Basket basket = null;
+
+        if (textFile.exists()) {
+            basket = Basket.loadFromBinFile(textFile);
+        } else {
+            basket = new Basket();
+        }
+
+        for (int i = 0; i < basket.getGoods().length; i++) {
+            System.out.println(i+1 + " " + basket.getGoods()[i] + " " + basket.getPrices()[i] + " руб/шт");
+        }
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Выберите товар и количество или введите `end`");
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                basket.printCart();
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            if (parts.length > 2 | parts.length == 1) {
+                System.out.println("Введите два значения через пробел и нажмите Enter!");
+                System.out.println();
+                continue;
+            }
+            try {
+                productNumber = Integer.parseInt(parts[0]);
+                if (productNumber <= 0 | productNumber > basket.getGoods().length) {
+                    System.out.println();
+                    System.out.println("Введите нормальный номер товара");
+                    System.out.println();
+                    continue;
+
+                }
+                productCount = Integer.parseInt(parts[1]);
+                if (productCount <= 0 | productCount > 100) {
+                    System.out.println();
+                    System.out.println("Введите нормальное количесвто товара!");
+                    System.out.println();
+                    continue;
+                }
+                basket.addToCart(productNumber, productCount);
+                basket.saveBin(textFile);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Вы ввели не число!");
+            } finally {
+                continue;
+            }
+        }
+    }
+}
